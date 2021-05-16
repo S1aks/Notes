9de0d9e37d571cb3c433e04a3ee06da568cd7f2f
Viacheslav Khrykin <vh78aks@gmail.com>
2021-05-16 11:31:17 +0300

    Init commit

---
+++ app/src/main/java/ru/s1aks/notes/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.s1aks.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
