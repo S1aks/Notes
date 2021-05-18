ad68c8cc0e7ae370f8e1d54ecbeddb192e0920be
Viacheslav Khrykin <vh78aks@gmail.com>
2021-05-18 19:24:25 +0300

    Add Data class & fragment

---
+++ app/src/main/java/ru/s1aks/notes/Note.java
@@ -0,0 +1,11 @@
+package ru.s1aks.notes;
+
+import java.util.GregorianCalendar;
+
+public class Note {
+    private int id;
+    private String title;
+    private String content;
+    private GregorianCalendar createTime;
+    private int importance;
+}
---
+++ app/src/main/java/ru/s1aks/notes/NotesListFragment.java
@@ -0,0 +1,32 @@
+package ru.s1aks.notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotesListFragment extends Fragment {
+
+    public NotesListFragment() {
+        // Required empty public constructor
+    }
+
+    public static NotesListFragment newInstance() {
+        return new NotesListFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    }
+}
