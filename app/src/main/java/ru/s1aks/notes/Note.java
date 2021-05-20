6c64cf46de56e82eceb6a33611e9ea902e1cb476
Viacheslav Khrykin <vh78aks@gmail.com>
2021-05-20 05:35:58 +0300

    Checkpoint 1

--- app/src/main/java/ru/s1aks/notes/Note.java
+++ app/src/main/java/ru/s1aks/notes/Note.java
@@ -8,4 +8,67 @@ public class Note {
     private String content;
     private GregorianCalendar createTime;
     private int importance;
+
+    private static int freeId;
+
+    public Note(String title, String content, GregorianCalendar createTime, int importance) {
+        this.id = freeId++;
+        this.title = title;
+        this.content = content;
+        this.createTime = createTime;
+        this.importance = importance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Note setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Note setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Note setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public GregorianCalendar getCreateTime() {
+        return createTime;
+    }
+
+    public Note setCreateTime(GregorianCalendar createTime) {
+        this.createTime = createTime;
+        return this;
+    }
+
+    public int getImportance() {
+        return importance;
+    }
+
+    public Note setImportance(int importance) {
+        this.importance = importance;
+        return this;
+    }
+
+    public static int getFreeId() {
+        return freeId;
+    }
+
+    public static void setFreeId(int freeId) {
+        Note.freeId = freeId;
+    }
 }
--- app/src/main/java/ru/s1aks/notes/NotesListFragment.java
+++ app/src/main/java/ru/s1aks/notes/NotesListFragment.java
@@ -1,21 +1,25 @@
 package ru.s1aks.notes;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class NotesListFragment extends Fragment {
 
-    public NotesListFragment() {
-        // Required empty public constructor
-    }
+    ArrayList<Note> notes;
 
-    public static NotesListFragment newInstance() {
-        return new NotesListFragment();
+    public NotesListFragment() {
     }
 
     @Override
@@ -26,7 +30,49 @@ public class NotesListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        createNotesList();
+        initList(view);
+    }
+
+    private void createNotesList() {
+        notes = new ArrayList<>();
+        notes.add(new Note("Title 1", "Content 1",
+                new GregorianCalendar(2021, 5, 2, 11, 34),
+                1));
+        notes.add(new Note("Title 2", "Content 2",
+                new GregorianCalendar(2021, 5, 2, 14, 6),
+                1));
+        notes.add(new Note("Title 3", "Content 3",
+                new GregorianCalendar(2021, 5, 3, 19, 15),
+                1));
+        notes.add(new Note("Title 4", "Content 4",
+                new GregorianCalendar(2021, 5, 5, 22, 54),
+                1));
+        notes.add(new Note("Title 5", "Content 5",
+                new GregorianCalendar(2021, 5, 8, 23, 38),
+                1));
+        notes.add(new Note("Title 6", "Content 6",
+                new GregorianCalendar(2021, 5, 10, 8, 2),
+                1));
+        notes.add(new Note("Title 7", "Content 7",
+                new GregorianCalendar(2021, 5, 12, 14, 31),
+                1));
+    }
+
+    private void initList(View view) {
+        LinearLayout linearLayout = (LinearLayout) view;
+        for (Note note : notes) {
+            TextView textView = new TextView(this.getContext());
+            textView.setText(note.getTitle());
+            textView.setTextSize(30);
+            linearLayout.addView(textView);
+            linearLayout.requestLayout();
+        }
     }
 }
